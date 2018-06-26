c2b4ad4372d7890b7ffa1e1eb17d358ea3abac3f
larwas <164722305@qq.com>
2018-06-26 11:41:39 +0800

    创建订单

--- database/migrations/2018_06_26_014334_create_orders_table.php
+++ database/migrations/2018_06_26_014334_create_orders_table.php
@@ -24,11 +24,11 @@ class CreateOrdersTable extends Migration
             $table->dateTime('paid_at')->nullable();
             $table->string('payment_method')->nullable();
             $table->string('payment_no')->nullable();
-            $table->string('refund_status');
+            $table->string('refund_status')->default(\App\Models\Order::REFUND_STATUS_PENDING);
             $table->string('refund_no')->nullable();
             $table->boolean('closed')->default(false);
             $table->boolean('reviewed')->default(false);
-            $table->string('ship_status');
+            $table->string('ship_status')->default(\App\Models\Order::SHIP_STATUS_PENDING);
             $table->text('ship_data')->nullable();
             $table->text('extra')->nullable();
             $table->timestamps();
