bb542ed4c9dd79f245a9aaa80e83f7307be6c992
larwas <164722305@qq.com>
2018-06-27 10:45:36 +0800

    集成微信支付

--- config/pay.php
+++ config/pay.php
@@ -11,11 +11,11 @@ return [
     ],
 
     'wechat' => [
-        'app_id'      => '',
-        'mch_id'      => '',
-        'key'         => '',
-        'cert_client' => '',
-        'cert_key'    => '',
+        'app_id'      => 'wx*******',   // 公众号 app id
+        'mch_id'      => '14*****',  // 第一步获取到的商户号
+        'key'         => '******', // 刚刚设置的 API 密钥
+        'cert_client' => resource_path('wechat_pay/apiclient_cert.pem'),
+        'cert_key'    => resource_path('wechat_pay/apiclient_key.pem'),
         'log'         => [
             'file' => storage_path('logs/wechat_pay.log'),
         ],
