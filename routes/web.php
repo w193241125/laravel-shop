b2fd269662daac3b934296d8ad46c582754ae4ce
larwas <164722305@qq.com>
2018-06-27 16:32:33 +0800

    确认收货

--- routes/web.php
+++ routes/web.php
@@ -43,6 +43,8 @@ Route::group(['middleware' => 'auth'], function() {
         Route::get('payment/{order}/alipay', 'PaymentController@payByAlipay')->name('payment.alipay');
         Route::get('payment/alipay/return', 'PaymentController@alipayReturn')->name('payment.alipay.return');
         Route::get('payment/{order}/wechat', 'PaymentController@payByWechat')->name('payment.wechat');
+        Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
+        Route::post('orders/{order}/received', 'OrdersController@received')->name('orders.received');
     });
     // 结束
 });
