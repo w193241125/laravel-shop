7507c113f9bd2dab87b06acd9fdbee0267c907d1
larwas <164722305@qq.com>
2018-06-28 12:05:17 +0800

    用户界面 - 使用优惠券下单

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -15,6 +15,7 @@ class Handler extends ExceptionHandler
     protected $dontReport = [
         //
         InvalidRequestException::class,
+        CouponCodeUnavailableException::class,
     ];
 
     /**
--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Events\OrderReviewd;
+use App\Exceptions\CouponCodeUnavailableException;
 use App\Http\Requests\ApplyRefundRequest;
+use App\Models\CouponCode;
 use Carbon\Carbon;
 use App\Http\Requests\SendReviewRequest;
 use App\Http\Requests\OrderRequest;
@@ -30,8 +32,16 @@ class OrdersController extends Controller
     {
         $user    = $request->user();
         $address = UserAddress::find($request->input('address_id'));
-
-        return $orderService->store($user, $address, $request->input('remark'), $request->input('items'));
+        $coupon  = null;
+        // 如果用户提交了优惠码
+        if ($code = $request->input('coupon_code')) {
+            $coupon = CouponCode::where('code', $code)->first();
+            if (!$coupon) {
+                throw new CouponCodeUnavailableException('优惠券不存在');
+            }
+        }
+        // 参数中加入 $coupon 变量
+        return $orderService->store($user, $address, $request->input('remark'), $request->input('items'), $coupon);
     }
 
     public function show(Order $order, Request $request)
